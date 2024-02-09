75c5e304fcb0ba865a9735b65b2f8ce0d924ece9
Matouš Suchý <matous.suchy33@gmail.com>
2024-02-09 13:10:47 +0100

    nastenka

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -11,10 +11,7 @@ class index
 
     public function install(\Base $base)
     {
-        \models\pins::setup();
-        \models\pins::setdown();
-
-        echo ('nainstalovano');
+        echo('sorry nefunguje');
 
     }
 
@@ -34,5 +31,35 @@ class index
         }
         echo json_encode($result);
     }
+
+    public function postAddPin(\Base $base)
+    {
+        $content = $base->get("POST.content");
+        $author = $base->get("POST.author");
+        $color = $base->get("POST.color");
+
+        // Remove the first character from the color (assuming it's a '#')
+        $color = substr($color, 1);
+
+        $pin = new \models\pins();
+        $pin->content = $content;
+        $pin->author = $author;
+        $pin->color = $color;
+        $pin->save();
+        // Save the pin
+        echo json_encode(['success' => true]);
+
+    }
+
+    public function delete(\Base $base)
+    {
+        $id = $base->get("POST.id");
+        $pinmodel = new \models\pins();
+        $pin = $pinmodel->findone(['id=?', $id]);
+        $pin->erase();
+        echo json_encode(['succes' => true]);
+
+    }
+
 
 }
